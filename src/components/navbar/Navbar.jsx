46bb8609efdcfb052cd20e90f30b3309648c55b2
Salvatore <salvatore.didio@dgccrf.finances.gouv.fr>
2024-05-24 08:08:11 +0200

    Add SideBarre

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import Links from "./links";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div>Logo</div>
+      <div>
+        <Links />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/navbar/links.jsx
@@ -0,0 +1,36 @@
+const { default: Link } = require("next/link")
+
+const Links = () => {
+    const links = [
+        {
+            title : "Home",
+            path : '/'
+        },
+        {
+            title : "About",
+            path : '/about'
+        },
+        {
+            title : "Contact",
+            path : '/contact'
+        },
+        {
+            title : "Blog",
+            path : '/blog'
+        }
+    ]
+
+    return (
+        <div>
+            {links.map((link => {
+               return (  
+                    <Link href={link.path} key={link.title}>
+                        {link.title}
+                    </Link>
+               )
+            }))}
+        </div>
+    )
+}
+
+export default Links
